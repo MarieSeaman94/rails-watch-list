d324f521f50fda37a1f43e54c8ba920679c414e4
marie seaman <marie.seaman1202@gmail.com>
2022-07-09 13:50:38 +0900

    seed done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,4 @@
 class Movie < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
+  validates :title, presence: true, uniqueness: true
   validates :overview, presence: true
 end
